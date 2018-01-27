82d7a0f5cec8fcc6b7e1106c5523d615374920cf
Felipe Apablaza <fapablaza@devware.cl>
2018-01-27 01:42:42 -0300

    Added ShaderGL implementation (WIP)

--- src/XE.OpenGLCore/XE/Graphics/GL/ShaderGL.cpp
+++ src/XE.OpenGLCore/XE/Graphics/GL/ShaderGL.cpp
@@ -2,5 +2,49 @@
 #include "ShaderGL.hpp"
 
 namespace XE::Graphics::GL {
+    static GLenum g_shaderTypesGL[] = {
+        GL_VERTEX_SHADER,
+        GL_FRAGMENT_SHADER
+    };
 
+    ShaderGL::ShaderGL(const ShaderType type, const std::string &source) {
+        m_type = type;
+        m_source = source;
+        m_typeGL = g_shaderTypesGL[type];
+
+        m_id = ::glCreateShader(m_typeGL);
+
+        auto source = static_cast<const GLchar *const>(glsl.c_str());
+        auto size = static_cast<GLsizei>(glsl.size());
+        glShaderSource (m_id, 1, &source, &size);
+        glCompileShader(m_id);
+
+        // check for errors
+        GLint status;
+        glGetShaderiv(m_id, GL_COMPILE_STATUS, &status);
+
+        if (status == static_cast<GLint>(GL_FALSE)) {
+            const GLint logsize = 4096;
+            char buffer[logsize] = {};
+                
+            glGetShaderInfoLog(m_id, logsize, nullptr, buffer);
+                
+            std::cerr << buffer << std::endl;
+
+            throw std::runtime_error(buffer);
+        }
+
+    }
+
+    ShaderGL::~ShaderGL() {
+
+    }
+
+    ShaderType ShaderGL::GetType() const {
+        return m_type;
+    }
+
+    std::string ShaderGL::GetSource() const {
+        return m_source;
+    }
 }
--- src/XE.OpenGLCore/XE/Graphics/GL/ShaderGL.hpp
+++ src/XE.OpenGLCore/XE/Graphics/GL/ShaderGL.hpp
@@ -2,4 +2,27 @@
 #ifndef __XE_GRAPHICS_GL_SHADERGL_HPP__
 #define __XE_GRAPHICS_GL_SHADERGL_HPP__
 
+#include <XE/Graphics/Shader.hpp>
+
+namespace XE::Graphics::GL {
+    class ShaderGL : public Shader {
+    public:
+        ShaderGL(const ShaderType type, const std::string &source);
+
+        virtual ~ShaderGL();
+
+        virtual ShaderType GetType() const override;
+
+        virtual std::string GetSource() const override;
+
+    private:
+        ShaderType m_type;
+        std::string m_source;
+
+        GLenum m_typeGL;
+        
+
+    };
+}
+
 #endif
