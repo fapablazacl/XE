017a9e5a3bba80543b6bc8ab5f10bfba0b3d2a46
Felipe Apablaza <ing.apablaza@gmail.com>
2019-02-11 22:38:59 -0300

    Updated XE.hpp header

--- src/XE.Core/XE.hpp
+++ src/XE.Core/XE.hpp
@@ -2,8 +2,13 @@
 #ifndef __XE_HPP__
 #define __XE_HPP__
 
-#include <XE/Buffer.hpp>
+#include <XE/Predef.hpp>
 #include <XE/Stream.hpp>
 #include <XE/DataType.hpp>
+#include <XE/FileStream.hpp>
+#include <XE/MemoryStream.hpp>
+#include <XE/Buffer.hpp>
+#include <XE/Core.hpp>
+#include <XE/StreamSource.hpp>
 
 #endif
