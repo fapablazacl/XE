076f204115740f8e39c041182dfb639697473829
Felipe Apablaza <ing.apablaza@gmail.com>
2019-12-08 10:22:06 -0300

    Added initial ES2 Driver baseline (copied from the GL Desktop driver

---
+++ src/XE.Graphics.ES2/XE/Graphics/ES2/BufferES2.cpp
@@ -0,0 +1,42 @@
+
+#include "BufferES2.hpp"
+#include "Conversion.hpp"
+#include <cstdint>
+
+namespace XE {
+    BufferES2::BufferES2(const BufferDescriptor &desc) {
+        const GLenum targetGL = convertToGL(desc.type);
+        const GLenum usageGL = convertToGL(desc.usage, desc.access);
+
+        ::glGenBuffers(1, &m_id);
+        ::glBindBuffer(targetGL, m_id);
+        ::glBufferData(targetGL, desc.size, desc.data, usageGL);
+        ::glBindBuffer(targetGL, 0);
+
+        m_size = desc.size;
+        m_target = targetGL;
+        m_usage = usageGL;
+    }
+
+    BufferES2::~BufferES2() {
+        if (m_id) {
+            ::glDeleteBuffers(1, &m_id);
+        }
+    }
+
+    void BufferES2::read(std::byte* destination, const int size, const int offset, const int destinationOffset) const {
+        const int finalSize = size ? size : m_size;
+
+        ::glBindBuffer(m_target, m_id);
+        ::glGetBufferSubData(m_target, offset, finalSize, &destination[destinationOffset]);
+        ::glBindBuffer(m_target, 0);
+    }
+
+    void BufferES2::write(const std::byte *source, const int size, const int offset, const int sourceOffset) {
+        const int finalSize = size ? size : m_size;
+
+        ::glBindBuffer(m_target, m_id);
+        ::glBufferSubData(m_target, offset, finalSize, &source[sourceOffset]);
+        ::glBindBuffer(m_target, 0);
+    }
+}
---
+++ src/XE.Graphics.ES2/XE/Graphics/ES2/Texture2DES2.hpp
@@ -0,0 +1,35 @@
+
+#ifndef __XE_GRAPHICS_GL_TEXTURE2DGL_HPP__
+#define __XE_GRAPHICS_GL_TEXTURE2DGL_HPP__
+
+#include <XE/Graphics/Texture2D.hpp>
+
+#include "TextureBaseGL.hpp"
+
+namespace XE {
+    class Texture2DGL : public Texture2D, public TextureBaseGL {
+    public:
+        Texture2DGL(const PixelFormat format, const Vector2i &size, const PixelFormat sourceFormat, const DataType sourceDataType, const void *sourceData);
+
+        virtual ~Texture2DGL();
+
+        virtual PixelFormat getFormat() const override {
+            return m_format;
+        }
+        
+    public:
+        virtual Vector2i getSize() const override {
+            return m_size;
+        }
+        
+        virtual void setData(const std::byte *surfaceData, const int mipLevel, const PixelFormat surfaceFormat, const DataType surfaceDataType, const Recti &area) override;
+
+        virtual void getData(std::byte *surfaceData, const int mipLevel, const PixelFormat surfaceFormat, const DataType surfaceDataType) const override;
+
+    private:
+        PixelFormat m_format;
+        Vector2i m_size;
+    };
+}
+
+#endif
