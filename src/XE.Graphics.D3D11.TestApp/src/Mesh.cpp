9df675c8b3db6c3e5920bee54b9fc8ed8c598dd8
Felipe Apablaza <ing.apablaza@gmail.com>
2021-10-11 16:43:53 -0300

    Normalized Direct3D11 module

---
+++ src/XE.Graphics.D3D11.TestApp/src/Mesh.cpp
@@ -0,0 +1,106 @@
+
+#include "Mesh.h"
+#include <XE/Math/Vector.h>
+
+namespace TestApp {
+    using namespace XE;
+
+    struct VertexType {
+        Vector3f position;
+        Vector4f color;
+    };
+
+    Mesh::Mesh() {}
+
+    Mesh::~Mesh() {
+        this->Shutdown();
+    }
+
+    bool Mesh::Initialize(ID3D11Device *device) {
+        return this->InitializeBuffers(device);
+    }
+
+    void Mesh::Shutdown() {
+        this->ShutdownBuffers();
+    }
+
+    void Mesh::Render(ID3D11DeviceContext *context) {
+        this->RenderBuffers(context);
+    }
+
+    int Mesh::GetIndexCount() const {
+        return indexCount;
+    }
+        
+    bool Mesh::InitializeBuffers(ID3D11Device *device) {
+        VertexType vertices[] = {
+            {
+                {-1.0f, -1.0f, 0.0f}, 
+                {1.0f, 0.0f, 0.0f, 1.0f}
+            },
+            {
+                {0.0f, 1.0f, 0.0f}, 
+                {1.0f, 0.0f, 0.0f, 1.0f}
+            },
+            {
+                {1.0f, -1.0f, 0.0f}, 
+                {1.0f, 0.0f, 0.0f, 1.0f}
+            }
+        };
+
+        int indices[] = {0, 1, 2};
+
+        // setup vertex data
+        D3D11_BUFFER_DESC vertexBufferDesc = {};
+        vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+        vertexBufferDesc.ByteWidth = sizeof(VertexType) * 3;
+        vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+
+        D3D11_SUBRESOURCE_DATA vertexData = {};
+        vertexData.pSysMem = vertices;
+        vertexData.SysMemPitch = 0;
+        vertexData.SysMemSlicePitch = 0;
+
+        if (FAILED(device->CreateBuffer(&vertexBufferDesc, &vertexData, &vertexBuffer))) {
+            return false;
+        }
+
+        //setup index data
+        D3D11_BUFFER_DESC indexBufferDesc = {};
+        indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+        indexBufferDesc.ByteWidth = sizeof(int) * 3;
+        indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+
+        D3D11_SUBRESOURCE_DATA indexData = {};
+        indexData.pSysMem = indices;
+        indexData.SysMemPitch = 0;
+        indexData.SysMemSlicePitch = 0;
+
+        if (FAILED(device->CreateBuffer(&indexBufferDesc, &indexData, &indexBuffer))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    void Mesh::ShutdownBuffers() {
+        if (indexBuffer) {
+            indexBuffer->Release();
+            indexBuffer = nullptr;
+        }
+
+        if (vertexBuffer) {
+            vertexBuffer->Release();
+            vertexBuffer = nullptr;
+        }
+    }
+
+    void Mesh::RenderBuffers(ID3D11DeviceContext *context) {
+        const unsigned int stride = sizeof(VertexType);
+        const unsigned int offset = 0;
+
+        context->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
+        context->IASetIndexBuffer(indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+        context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    }
+}
---
+++ src/XE.Graphics.D3D11.TestApp/src/Shader.h
@@ -0,0 +1,46 @@
+
+#ifndef __XE_GRAPHICS_D3D11_TESTAPP_SHADER_HPP__
+#define __XE_GRAPHICS_D3D11_TESTAPP_SHADER_HPP__
+
+#include <d3d11.h>
+#include <XE/Math/Matrix.h>
+
+namespace TestApp {
+    class Shader {
+    public:
+        Shader();
+
+        ~Shader();
+
+        bool Initialize(ID3D11Device *device, HWND hWnd);
+
+        void Shutdown();
+
+        bool Render(ID3D11DeviceContext *context, int indexCount, const Matrix4f &world, const Matrix4f &view, const Matrix4f &projection);
+
+    private:
+        bool InitializeShader(ID3D11Device *device, HWND hWnd, LPCWSTR vertexShaderSrc, LPCWSTR pixelShaderSrc);
+
+        void ShutdownShader();
+
+        void DisplayShaderErrorMessage(ID3D10Blob *blob, HWND hWnd, LPCWSTR msg);
+
+        bool SetShaderParameters(ID3D11DeviceContext *context, const Matrix4f &world, const Matrix4f &view, const Matrix4f &projection);
+
+        void RenderShader(ID3D11DeviceContext *context, int indexCount);
+
+    private:
+        struct MatrixBufferType {
+            Matrix4f world;
+            Matrix4f view;
+            Matrix4f projection;
+        };
+
+        ID3D11VertexShader *vertexShader = nullptr;
+        ID3D11PixelShader *pixelShader = nullptr;
+        ID3D11InputLayout *layout = nullptr;
+        ID3D11Buffer *matrixBuffer = nullptr;
+    };
+}
+
+#endif
