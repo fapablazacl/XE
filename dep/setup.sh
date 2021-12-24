6078fb5b10b27932ddcc085dedaaed55b4e0b078
Felipe Apablaza <ing.apablaza@gmail.com>
2021-12-24 17:45:36 -0300

    added glbinding submodule. fixed some compilation errors

--- dep/setup.sh
+++ dep/setup.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
 export CC=gcc 
-export CXX=gcc 
+export CXX=g++
 
 # build catch
-cmake -Bdep/.build/Catch2 -Sdep/Catch2 -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=local -DCATCH_BUILD_TESTING=OFF -DCATCH_INSTALL_DOCS=OFF
+cmake -Bdep/.build/Catch2 -Sdep/Catch2 -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=local/Catch2 -DCATCH_BUILD_TESTING=OFF -DCATCH_INSTALL_DOCS=OFF
 cmake --build dep/.build/Catch2
 cmake --install dep/.build/Catch2
 
 # build glades2
-cmake -Bdep/.build/glades2/debug -Sdep/glades2 -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=local
+cmake -Bdep/.build/glades2/debug -Sdep/glades2 -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=local/glades2
 cmake --build dep/.build/glades2/debug
 cmake --install dep/.build/glades2/debug
 
-cmake -Bdep/.build/glades2/release -Sdep/glades2 -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=local
+cmake -Bdep/.build/glades2/release -Sdep/glades2 -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=local/glades2
 cmake --build dep/.build/glades2/release
 cmake --install dep/.build/glades2/release
 
 # build glfw
-cmake -Bdep/.build/glfw/debug -Sdep/glfw -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=local -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF
+cmake -Bdep/.build/glfw/debug -Sdep/glfw -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=local/glfw -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF
 cmake --build dep/.build/glfw/debug
 cmake --install dep/.build/glfw/debug
 
-cmake -Bdep/.build/glfw/release -Sdep/glfw -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=local -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF
+cmake -Bdep/.build/glfw/release -Sdep/glfw -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=local/glfw -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF
 cmake --build dep/.build/glfw/release
 cmake --install dep/.build/glfw/release
+
+# build glbinding
+cmake -Bdep/.build/glbinding/debug -Sdep/glbinding -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=local/glbinding -DOPTION_BUILD_EXAMPLES=OFF -DOPTION_BUILD_TOOLS=OFF -DBUILD_SHARED_LIBS=OFF
+cmake --build dep/.build/glbinding/debug
+cmake --install dep/.build/glbinding/debug
+
+cmake -Bdep/.build/glbinding/release -Sdep/glbinding -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=local/glbinding -DOPTION_BUILD_EXAMPLES=OFF -DOPTION_BUILD_TOOLS=OFF -DBUILD_SHARED_LIBS=OFF
+cmake --build dep/.build/glbinding/release
+cmake --install dep/.build/glbinding/release
---
+++ setup-unix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export CC=gcc 
+export CXX=g++
+
+cmake -B build/gcc/debug \
+    -G "Unix Makefiles" \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DXE_PLUGIN_GL=ON -DXE_PLUGIN_ES2=ON -DXE_PLUGIN_GL_GLFW=ON -DXE_PLUGIN_VULKAN=ON -DXE_UNIT_TEST=ON \
+    -Dglfw3_DIR=$PWD/local/glfw/lib/cmake/glfw3 \
+    -Dglbinding_DIR=$PWD/local/glbinding \
+    -Dglades2_DIR=$PWD/local/glades2/lib/cmake/glades2 \
+    -DCatch2_DIR=$PWD/local/Catch2/lib/cmake/Catch2
