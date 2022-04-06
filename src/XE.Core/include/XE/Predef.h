f0d9affff2392c06a11072c7ab85e00656e0becb
Felipe Apablaza <ing.apablaza@gmail.com>
2022-04-06 09:13:33 -0400

    added os detection at build time

--- src/XE.Core/include/XE/Predef.h
+++ src/XE.Core/include/XE/Predef.h
@@ -5,6 +5,36 @@
 #include <cstdint>
 #include <cstddef>
 
+//! API export/import definition.
+//! Defined when we are using a DLL/Shared Object/DYLIB.
 #define XE_API
+
+//! detect target OS 
+#if defined(_WINDOWS)
+#define XE_OS_WINDOWS
+#elif defined(__linux__)
+#define XE_OS_LINUX
+#elif __APPLE__
+#include <TargetConditionals.h>
+#if TARGET_IPHONE_SIMULATOR
+#define XE_OS_IOS_SIMULATOR
+#elif TARGET_OS_IPHONE
+#define XE_OS_IOS
+#elif TARGET_OS_MAC
+#define XE_OS_MACOS
+#else
+#define XE_OS_UNKNOWN
+#endif
+#elif __ANDROID__
+#define XE_OS_ANDROID
+#elif __FreeBSD__
+#define XE_OS_FREEBSD
+#else
+#define XE_OS_UNKNOWN
+#endif
+
+#if XE_OS_UNKNOWN
+#pragma message "Unknown OS isn't Supported. You are on your own.'"
+#endif
 
 #endif
