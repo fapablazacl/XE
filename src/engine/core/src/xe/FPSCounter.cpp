041418f86fa7531e3406a90e9474461a288a10ac
Felipe Apablaza <felipe.apablaza@protonmail.com>
2023-06-22 07:48:03 -0400

    moved FPSCounter class to the engine core

---
+++ src/engine/core/src/xe/FPSCounter.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Felipe Apablaza on 26-03-23.
+//
+
+#include "FPSCounter.h"
+
+namespace XE {
+    bool FPSCounter::frame() {
+        const int milliseconds = XE::Timer::getTick() - lastTime;
+
+        if (milliseconds < 1000) {
+            currentFrames++;
+
+            return false;
+        } else {
+            fps = currentFrames;
+            currentFrames = 0;
+            lastTime = XE::Timer::getTick();
+
+            return true;
+        }
+    }
+}
