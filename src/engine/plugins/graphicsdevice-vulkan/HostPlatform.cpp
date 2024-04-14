ce6ee42e51aeccc91be364c55f541a7e9215e80f
Felipe Apablaza <felipe.apablaza@protonmail.com>
2024-04-14 11:04:01 -0400

    moved method definitions in headers to cpp files

--- src/engine/plugins/graphicsdevice-vulkan/HostPlatform.cpp
+++ src/engine/plugins/graphicsdevice-vulkan/HostPlatform.cpp
@@ -1,3 +1,99 @@
 
 #include "HostPlatform.h"
 
+
+HostPlatform::HostPlatform(const std::string &title, const uint32_t screenWidth, const uint32_t screenHeight, const HostPlatformFlagBits flags)
+    : mScreenWidth(screenWidth), mScreenHeight(screenHeight), mFlags(flags) {
+    assert(screenWidth > 0);
+    assert(screenHeight > 0);
+
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    mWindow = glfwCreateWindow(mScreenWidth, mScreenHeight, title.c_str(), nullptr, nullptr);
+}
+
+HostPlatform::~HostPlatform() {
+    destroyWindow();
+    glfwTerminate();
+}
+
+bool HostPlatform::closeWasRequested() const { return glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS || glfwWindowShouldClose(mWindow) == GLFW_TRUE; }
+
+uint32_t HostPlatform::getScreenWidth() const { return mScreenWidth; }
+
+uint32_t HostPlatform::getScreenHeight() const { return mScreenHeight; }
+
+std::vector<const char *> HostPlatform::enumerateRequiredInstanceExtensions() const {
+    uint32_t extensionCount = 0;
+    const char **extensions = glfwGetRequiredInstanceExtensions(&extensionCount);
+
+    std::vector<const char *> result = {extensions, extensions + extensionCount};
+
+    result.push_back("VK_KHR_get_physical_device_properties2");
+
+    if (mFlags == HostPlatformFlagBits::Debug) {
+        result.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+
+    return result;
+}
+
+std::vector<const char *> HostPlatform::enumerateRequiredDeviceExtensions() const {
+    return {
+        // this extensions provides all the objects required for swapchains
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+
+        // required for platforms where Vulkan isn't supported directly by the OS
+#if defined(XE_OS_MACOS) || defined(XE_OS_IOS)
+            "VK_KHR_portability_subset"
+#endif
+    };
+}
+
+std::vector<const char *> HostPlatform::enumerateValidationLayers() const {
+    if (mFlags == HostPlatformFlagBits::Debug) {
+        return {"VK_LAYER_KHRONOS_validation"};
+    }
+
+    return {};
+}
+
+void HostPlatform::pollEvents() { glfwPollEvents(); }
+
+vk::Extent2D HostPlatform::pickSwapExtent(const vk::SurfaceCapabilitiesKHR &surfaceCaps) const {
+    assert(mWindow);
+
+    if (surfaceCaps.currentExtent.width != UINT32_MAX) {
+        return surfaceCaps.currentExtent;
+    }
+
+    int width, height;
+    glfwGetFramebufferSize(mWindow, &width, &height);
+
+    const vk::Extent2D actualExtent = {static_cast<uint32_t>(width), static_cast<uint32_t>(height)};
+
+    return {
+        std::clamp(actualExtent.width, surfaceCaps.minImageExtent.width, surfaceCaps.maxImageExtent.width),
+        std::clamp(actualExtent.height, surfaceCaps.minImageExtent.height, surfaceCaps.maxImageExtent.height),
+    };
+}
+
+vk::SurfaceKHR HostPlatform::createSurface(vk::Instance &instance) const {
+    VkSurfaceKHR rawsurf;
+
+    if (glfwCreateWindowSurface(instance, mWindow, nullptr, &rawsurf) != VK_SUCCESS) {
+        return {};
+    }
+
+    return rawsurf;
+}
+
+
+void HostPlatform::destroyWindow() {
+    if (mWindow) {
+        glfwDestroyWindow(mWindow);
+        mWindow = nullptr;
+    }
+}
--- src/engine/plugins/graphicsdevice-vulkan/HostPlatform.h
+++ src/engine/plugins/graphicsdevice-vulkan/HostPlatform.h
@@ -9,103 +9,31 @@ enum class HostPlatformFlagBits { None = 0x00, Debug = 0x01 };
 
 class HostPlatform {
 public:
-    explicit HostPlatform(const std::string &title, const uint32_t screenWidth, const uint32_t screenHeight, const HostPlatformFlagBits flags)
-        : mScreenWidth(screenWidth), mScreenHeight(screenHeight), mFlags(flags) {
-        assert(screenWidth > 0);
-        assert(screenHeight > 0);
+    explicit HostPlatform(const std::string& title, const uint32_t screenWidth, const uint32_t screenHeight, const HostPlatformFlagBits flags);
 
-        glfwInit();
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    ~HostPlatform();
 
-        mWindow = glfwCreateWindow(mScreenWidth, mScreenHeight, title.c_str(), nullptr, nullptr);
-    }
+    bool closeWasRequested() const;
 
-    ~HostPlatform() {
-        destroyWindow();
-        glfwTerminate();
-    }
+    uint32_t getScreenWidth() const;
 
-    bool closeWasRequested() const { return glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS || glfwWindowShouldClose(mWindow) == GLFW_TRUE; }
+    uint32_t getScreenHeight() const;
 
-    uint32_t getScreenWidth() const { return mScreenWidth; }
+    std::vector<const char*> enumerateRequiredInstanceExtensions() const;
 
-    uint32_t getScreenHeight() const { return mScreenHeight; }
+    std::vector<const char*> enumerateRequiredDeviceExtensions() const;
 
-    std::vector<const char *> enumerateRequiredInstanceExtensions() const {
-        uint32_t extensionCount = 0;
-        const char **extensions = glfwGetRequiredInstanceExtensions(&extensionCount);
+    std::vector<const char*> enumerateValidationLayers() const;
 
-        std::vector<const char *> result = {extensions, extensions + extensionCount};
+    void pollEvents();
 
-        result.push_back("VK_KHR_get_physical_device_properties2");
+    vk::Extent2D pickSwapExtent(const vk::SurfaceCapabilitiesKHR& surfaceCaps) const;
 
-        if (mFlags == HostPlatformFlagBits::Debug) {
-            result.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-        }
-
-        return result;
-    }
-
-    std::vector<const char *> enumerateRequiredDeviceExtensions() const {
-        return {
-            // this extensions provides all the objects required for swapchains
-            VK_KHR_SWAPCHAIN_EXTENSION_NAME,
-
-            // required for platforms where Vulkan isn't supported directly by the OS
-#if defined(XE_OS_MACOS) || defined(XE_OS_IOS)
-                "VK_KHR_portability_subset"
-#endif
-        };
-    }
-
-    std::vector<const char *> enumerateValidationLayers() const {
-        if (mFlags == HostPlatformFlagBits::Debug) {
-            return {"VK_LAYER_KHRONOS_validation"};
-        }
-
-        return {};
-    }
-
-    void pollEvents() { glfwPollEvents(); }
-
-    vk::Extent2D pickSwapExtent(const vk::SurfaceCapabilitiesKHR &surfaceCaps) const {
-        assert(mWindow);
-
-        if (surfaceCaps.currentExtent.width != UINT32_MAX) {
-            return surfaceCaps.currentExtent;
-        }
-
-        int width, height;
-        glfwGetFramebufferSize(mWindow, &width, &height);
-
-        const vk::Extent2D actualExtent = {static_cast<uint32_t>(width), static_cast<uint32_t>(height)};
-
-        return {
-            std::clamp(actualExtent.width, surfaceCaps.minImageExtent.width, surfaceCaps.maxImageExtent.width),
-            std::clamp(actualExtent.height, surfaceCaps.minImageExtent.height, surfaceCaps.maxImageExtent.height),
-        };
-    }
-
-    vk::SurfaceKHR createSurface(vk::Instance &instance) const {
-        VkSurfaceKHR rawsurf;
-
-        if (glfwCreateWindowSurface(instance, mWindow, nullptr, &rawsurf) != VK_SUCCESS) {
-            return {};
-        }
-
-        return rawsurf;
-    }
+    vk::SurfaceKHR createSurface(vk::Instance& instance) const;
 
 private:
-    void destroyWindow() {
-        if (mWindow) {
-            glfwDestroyWindow(mWindow);
-            mWindow = nullptr;
-        }
-    }
+    void destroyWindow();
 
-private:
     GLFWwindow *mWindow = nullptr;
     uint32_t mScreenWidth = 1024;
     uint32_t mScreenHeight = 768;
