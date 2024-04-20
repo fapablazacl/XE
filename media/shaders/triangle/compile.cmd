b4eed0652212ee1574958d6d7ea6bb1d44521c22
Felipe Apablaza <felipe.apablaza@protonmail.com>
2024-04-20 12:26:08 -0400

    built shaders

--- media/shaders/triangle/compile.cmd
+++ media/shaders/triangle/compile.cmd
@@ -1,4 +1,4 @@
 
-D:\Development\sdks\VulkanSDK\1.2.162.1\Bin\glslc.exe shader.vert -o vert.spv
-D:\Development\sdks\VulkanSDK\1.2.162.1\Bin\glslc.exe shader.frag -o frag.spv
+glslc.exe shader.vert -o vert.spv
+glslc.exe shader.frag -o frag.spv
 pause
