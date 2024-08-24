dedab935ca82712753ebc0bceaafb2f491dab629
Felipe Apablaza <ing.apablaza@gmail.com>
2024-08-24 09:24:45 -0400

    sdl opengl test

--- src/demos/capybaria/src/sandbox_main.cpp
+++ src/demos/capybaria/src/sandbox_main.cpp
@@ -1,30 +1,79 @@
 
-#include <array>
-#include <iostream>
 #include <cstdio>
-
-#include "SandboxApp.h"
 #include <xe/FPSCounter.h>
-
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_main.h>
+#include <SDL_opengl.h>
 
 
 int main(int argc, char *argv[]) {
     const int SCREEN_WIDTH = 640;
     const int SCREEN_HEIGHT = 480;
 
+    std::printf("Initializing SDL video subsystem\n");
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::printf("Error while initialising SDL: %s\n", SDL_GetError());
+        std::printf("Error while initialising SDL. SDL_Error: %s\n", SDL_GetError());
 
         return EXIT_FAILURE;
     }
 
+    std::printf("Creating diplay window with 640 x 480 mode, windowed mode\n");
+    auto window = SDL_CreateWindow( "Capybaria", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if( window == nullptr ) {
+        std::printf( "Error while creating Window. SDL_Error: %s\n", SDL_GetError() );
 
+        return EXIT_FAILURE;
+    }
 
+    //Use OpenGL 3.1 core
+    std::printf("Creating OpenGL context 3.1 core profile context\n");
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 1 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+
+    auto context = SDL_GL_CreateContext(window);
+    if (context == nullptr) {
+        std::printf( "Error while creating OpenGL context. SDL_Error: %s\n", SDL_GetError() );
+
+        return EXIT_FAILURE;
+    }
+
+    std::printf("OpenGL info:\n");
+    std::printf("GL_VENDOR: %s\n", glGetString(GL_VENDOR));
+    std::printf("GL_RENDERER: %s\n", glGetString(GL_RENDERER));
+    std::printf("GL_VERSION: %s\n", glGetString(GL_VERSION));
+    std::printf("GL_EXTENSIONS: %s\n", glGetString(GL_EXTENSIONS));
+
+    //Use Vsync
+    std::printf("Configuring swap interval\n");
+    if( SDL_GL_SetSwapInterval( 1 ) < 0 ) {
+        printf( "Warning: Unable to set VSync! SDL Error: %s\n", SDL_GetError() );
+
+        return EXIT_FAILURE;
+    }
+
+    //Hack to get window to stay up
+    std::printf("Entering main loop\n");
+    SDL_Event e;
+
+    bool quit = false;
+    while( !quit ) {
+        while( SDL_PollEvent( &e ) ) {
+            if( e.type == SDL_QUIT ) {
+                quit = true;
+            }
+        }
+
+        glClearColor(0.0f, 0.0f, 0.8f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glFlush();
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
-
+    /*
     try {
         XE::FPSCounter fpsCounter;
 
@@ -53,6 +102,7 @@ int main(int argc, char *argv[]) {
 
         return EXIT_FAILURE;
     }
+    */
 
     return EXIT_SUCCESS;
 }
