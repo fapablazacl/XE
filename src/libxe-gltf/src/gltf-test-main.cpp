f4fa30ae0129a8cedde0ce2035eaba2143810e7f
Felipe Apablaza <felipe.apablaza@protonmail.com>
2025-04-18 06:55:18 -0400

    added more exploratory statements

--- src/libxe-gltf/src/gltf-test-main.cpp
+++ src/libxe-gltf/src/gltf-test-main.cpp
@@ -18,6 +18,23 @@ static std::string to_string(cgltf_result result) {
     }
 }
 
+std::string evaluate_name(const char* name) {
+    if (name) {
+        return name;
+    }
+
+    return "";
+}
+
+template<typename T>
+std::string evaluate_ptr(const std::string &msg, const T* ptr) {
+    return msg + (ptr ? "true" : "false");
+}
+
+std::string evaluate_bool(const cgltf_bool value) {
+    return (value ? "true" : "false");
+}
+
 void process_camera(cgltf_camera *camera) {
     std::cout << "camera node" << std::endl;
 }
@@ -27,7 +44,16 @@ void process_light(cgltf_light *light) {
 }
 
 void process_material(const cgltf_material *material) {
-    std::cout << "material node" << std::endl;
+    std::cout << "Material name " << material->name << std::endl;
+
+    std::cout << "Material has clearcoat " << evaluate_bool(material->has_clearcoat) << std::endl;
+    std::cout << "Material has ior " << evaluate_bool(material->has_ior) << std::endl;
+    std::cout << "Material has metallic roughness " << evaluate_bool(material->has_pbr_metallic_roughness) << std::endl;
+    std::cout << "Material has specular glossiness " << evaluate_bool(material->has_pbr_specular_glossiness) << std::endl;
+    std::cout << "Material has sheen " << evaluate_bool(material->has_sheen) << std::endl;
+    std::cout << "Material has specular " << evaluate_bool(material->has_specular) << std::endl;
+    std::cout << "Material has transmission " << evaluate_bool(material->has_transmission) << std::endl;
+    std::cout << "Material has volume " << evaluate_bool(material->has_volume) << std::endl;
 }
 
 void process_buffer_view(const cgltf_buffer_view *view) {
@@ -113,16 +139,36 @@ void process_scene(cgltf_data *data, cgltf_scene *scene) {
 }
 
 void process_animation(cgltf_animation *animation) {
-
+    std::cout << "Animation name: " << evaluate_name(animation->name) << std::endl;
+    std::cout << "Animation samplers count: " << animation->samplers_count << std::endl;
+    std::cout << "Animation channels count: " << animation->channels_count << std::endl;
+    std::cout << "Animation extensions count: " << animation->extensions_count << std::endl;
 }
 
 void process_skins(cgltf_skin *skin) {
-
+    std::cout << "Skin name: " << evaluate_name(skin->name) << std::endl;
+    std::cout << evaluate_ptr("Skin has skeleton node: ", skin->skeleton) << std::endl;
+    std::cout << "Animation joints count: " << skin->joints_count << std::endl;
+    std::cout << "Animation extensions count: " << skin->extensions_count << std::endl;
 }
 
 void process_texture(cgltf_texture *texture) {
     std::cout << "Texture name: " << (texture->name ? texture->name : "<noname>")  << std::endl;
+    std::cout << "Texture extensions count: " << texture->extensions_count  << std::endl;
     std::cout << "Texture has sampler: " << (texture->sampler ? "true" : "false")  << std::endl;
+
+    const cgltf_sampler *sampler = texture->sampler;
+
+    if (sampler) {
+        std::cout << "Texture sampler name: " << (sampler->name ? sampler->name : "<noname>")  << std::endl;
+        std::cout << "Texture mag filter: " << sampler->mag_filter  << std::endl;
+        std::cout << "Texture min filter: " << sampler->min_filter  << std::endl;
+        std::cout << "Texture wrap s: " << sampler->wrap_s  << std::endl;
+        std::cout << "Texture wrap t: " << sampler->wrap_t  << std::endl;
+        std::cout << "Texture extensions count: " << sampler->extensions_count << std::endl;
+    }
+
+    std::cout << "Texture has basisu: " << (texture->has_basisu ? "true" : "false")  << std::endl;
 }
 
 void process_image(cgltf_image *image) {
@@ -130,6 +176,7 @@ void process_image(cgltf_image *image) {
     std::cout << "Image uri: " << (image->uri ? image->uri : "<noname>")  << std::endl;
     std::cout << "Image mime type: " << (image->mime_type ? image->mime_type : "<noname>")  << std::endl;
     std::cout << "Image has buffer view: " << (image->buffer_view ? "true" : "false")  << std::endl;
+    std::cout << "Image extensions count: " << image->extensions_count  << std::endl;
 }
 
 void process_sampler(cgltf_sampler *sampler) {
@@ -144,12 +191,12 @@ void process_data_extension(cgltf_extension *extension) {
 
 }
 
-void process_extensions_required(char **str) {
+void process_extensions_required(char *str) {
 
 }
 
-void process_extensions_used(char **str) {
-
+void process_extensions_used(char *str) {
+    std::cout << "Extension used " << str << std::endl;
 }
 
 void process_variant(cgltf_material_variant * material_variant) {
@@ -179,42 +226,49 @@ int main() {
     std::cout << "Found " << data->lights_count << " lights" << std::endl;
     for (cgltf_size i = 0; i < data->lights_count; i++) {
         process_light(data->lights + i);
+        std::cout << std::endl;
     }
     std::cout << std::endl;
 
     std::cout << "Found " << data->cameras_count << " cameras" << std::endl;
     for (cgltf_size i = 0; i < data->cameras_count; i++) {
         process_camera(data->cameras + i);
+        std::cout << std::endl;
     }
     std::cout << std::endl;
 
     std::cout << "Found " << data->meshes_count << " meshes" << std::endl;
     for (cgltf_size i = 0; i < data->meshes_count; i++) {
         process_mesh(data->meshes + i);
+        std::cout << std::endl;
     }
     std::cout << std::endl;
 
     std::cout << "Found " << data->animations_count << " animations" << std::endl;
     for (cgltf_size i = 0; i < data->animations_count; i++) {
         process_animation(data->animations + i);
+        std::cout << std::endl;
     }
     std::cout << std::endl;
 
     std::cout << "Found " << data->skins_count << " skins" << std::endl;
     for (cgltf_size i = 0; i < data->skins_count; i++) {
         process_skins(data->skins + i);
+        std::cout << std::endl;
     }
     std::cout << std::endl;
 
     std::cout << "Found " << data->textures_count << " textures" << std::endl;
     for (cgltf_size i = 0; i < data->textures_count; i++) {
         process_texture(data->textures + i);
+        std::cout << std::endl;
     }
     std::cout << std::endl;
 
     std::cout << "Found " << data->images_count << " images" << std::endl;
     for (cgltf_size i = 0; i < data->images_count; i++) {
         process_image(data->images + i);
+        std::cout << std::endl;
     }
     std::cout << std::endl;
 
@@ -250,13 +304,13 @@ int main() {
 
     std::cout << "Found " << data->extensions_required_count << " extensions required" << std::endl;
     for (cgltf_size i = 0; i < data->extensions_required_count; i++) {
-        process_extensions_required(data->extensions_required + i);
+        process_extensions_required(data->extensions_required[i]);
     }
     std::cout << std::endl;
 
     std::cout << "Found " << data->extensions_used_count << " extensions used" << std::endl;
     for (cgltf_size i = 0; i < data->extensions_used_count; i++) {
-        process_extensions_used(data->extensions_used + i);
+        process_extensions_used(data->extensions_used[i]);
     }
     std::cout << std::endl;
 
