648b8962fd9bfe10f1b9415f57bb83b59cc40717
Felipe Apablaza <felipe.apablaza@protonmail.com>
2026-03-31 15:33:11 -0300

    updated conanfile references

--- conanfile.py
+++ conanfile.py
@@ -30,10 +30,10 @@ class xeRecipe(ConanFile):
     def requirements(self):
         # self.requires("gtest/1.17.0")
         self.requires("imgui/1.92.2b")
-        self.requires("assimp/5.4.3")
+        self.requires("assimp/6.0.2")
         self.requires("glfw/3.4")
         self.requires("cgltf/1.13")
-        self.requires("fmt/10.2.1")
+        self.requires("fmt/[>=11 <12]")
         self.requires("lodepng/cci.20230410")
         self.requires("ms-gsl/4.2.0")
         self.requires("devil/1.8.0")
@@ -41,7 +41,7 @@ class xeRecipe(ConanFile):
         self.requires("nlohmann_json/3.12.0")
         self.requires("vulkan-loader/1.4.313.0")
         self.requires("glad/0.1.36", options={"spec": "gl", "gl_profile": "core", "gl_version": "4.6"})
-        self.requires("ktx/4.3.2")
+        self.requires("ktx/4.4.2")
         self.requires("cxxopts/3.3.1")
 
         # NOTE: sdl2 and sdl3 both don't build under ArchLinux
