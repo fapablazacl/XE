3cccfdc4c44a44fb92bee6762c27257f351f796d
Felipe Apablaza <felipe.apablaza@protonmail.com>
2026-05-05 17:02:50 -0400

    removed DataType deprecation

--- src/libxe-core/src/xe/DataType.h
+++ src/libxe-core/src/xe/DataType.h
@@ -182,7 +182,6 @@ namespace xe {
     constexpr TypeEncoding Mat4x3_Float32 = makeMatrixType(TypeSize::Byte4, TypeKind::Float, 4, 3);
 
     enum class
-    [[deprecated]]
     DataType : std::int16_t { MetaFirst, Unknown = MetaFirst, UInt8, UInt16, UInt32, UInt64, Int8, Int16, Int32, Int64, Float16, Float32, Float64, MetaCount };
 
     [[deprecated]]
