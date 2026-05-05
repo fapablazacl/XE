db071f216fa6d7d703de87d96a650524132e0a6a
Felipe Apablaza <felipe.apablaza@protonmail.com>
2026-05-05 15:21:04 -0400

    added backport-cpp conan package

---
+++ conan/packages/backport-cpp/conanfile.py
@@ -0,0 +1,43 @@
+import os
+
+from conan import ConanFile
+from conan.tools.files import copy, get
+from conan.tools.layout import basic_layout
+
+required_conan_version = ">=2.0.0"
+
+
+class BackportCppConan(ConanFile):
+    name = "backport-cpp"
+    description = "An ongoing effort to bring modern C++ utilities to be compatible with C++11"
+    topics = ("header-only", "backport")
+    homepage = "https://github.com/bitwizeshift/BackportCpp"
+    url = "https://github.com/bitwizeshift/BackportCpp"
+    license = "MIT"
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
+    no_copy_source = True
+
+    def layout(self):
+        basic_layout(self, src_folder="src")
+
+    def package_id(self):
+        self.info.clear()
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def build(self):
+        pass
+
+    def package(self):
+        copy(self, "LICENSE", src=self.source_folder,
+             dst=os.path.join(self.package_folder, "licenses"))
+        copy(self, os.path.join("include", "**", "*.hpp"),
+             src=self.source_folder, dst=self.package_folder)
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "Backport")
+        self.cpp_info.set_property("cmake_target_name", "Backport::Backport")
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
---
+++ conan/packages/backport-cpp/test_package/test_backport.cpp
@@ -0,0 +1,8 @@
+#include <bpstd/string_view.hpp>
+#include <bpstd/span.hpp>
+
+int main() {
+    bpstd::string_view sv = "hello";
+    (void)sv;
+    return 0;
+}
